ff6753081b13f70f76ea980033a9e79b294c68e1
romi0330 <romina03villavicencio@gmail.com>
2023-05-18 11:57:25 -0500

    medio ejercicio

---
+++ matriz_en_cero.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    
+    int num1,num2;
+    printf("Ingrese numero de columnas: \n");
+    scanf("%d", &num1);
+    printf("Ingrese numero de filas: \n");
+    scanf("%d", &num2);
+    int matriz[num1][num2];
+    memset(matriz, 0, sizeof(matriz));
+
+    for (int i = 0; i < num2; i++) {
+        for (int j = 0; j < num1; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
